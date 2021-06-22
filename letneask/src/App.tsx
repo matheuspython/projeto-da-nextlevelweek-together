427e1dac37898b3a0b2b975cabb9694303b5c16e
matheus souza <ruivorobado@gmail.com>
2021-06-22 20:23:40 -0300

    erro do exact corrijido

--- letneask/src/App.tsx
+++ letneask/src/App.tsx
@@ -1,12 +1,15 @@
 //import { Home } from "./pages/Home";
-import {Route} from 'react-router-dom'
+import {BrowserRouter, Route} from 'react-router-dom'
+import { Home } from './pages/Home';
 import { NewRoom } from "./pages/NewRoom";
 
 
 function App() {
   return (
-  //  <Home />
-  <NewRoom />  
+    <BrowserRouter>
+      <Route exact path="/" component={Home}/>
+      <Route path={"/rooms/new"} component={NewRoom}/>
+    </BrowserRouter>
   );
 }
 
