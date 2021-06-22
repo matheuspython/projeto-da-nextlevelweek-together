6d7a5983f6fe00cf63c53d13735f1f4696e5bc92
matheus souza <ruivorobado@gmail.com>
2021-06-22 13:17:45 -0300

    tela de cadastro completa

--- letneask/src/services/Home.tsx
+++ letneask/src/services/Home.tsx
@@ -2,27 +2,34 @@ import ilustrationImg from '../assets/images/illustration.svg'
 import logoImg from "../assets/images/logo.svg"
 import googleiconconfig from "../assets/images/google-icon.svg"
 
+import {Button} from '../components/Button'
+
+import '../styles/auth.scss'
+
 export function Home() {
   return (
-    <div>
+    <div id="page-auth">
       <aside>
         <img src={ilustrationImg} alt="Ilustração simboliazndo perguntasa e respostas" />
         <strong>Crie salas de Q&amp;A ao-vivo</strong>
         <p>Tire as duvidas de sua audiência em tempo-real</p>
       </aside>
       <main>
-        <div>
+        <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button>
+          <button className="create-room">
             <img src={googleiconconfig} alt="Logo do google" />
             Crie sua sala com o Google
           </button>
-          <div>ou entre em uma sala</div>
+          <div className="separator">ou entre em uma sala</div>
           <form>
             <input
              type="text"
              placeholder="digite o código da sala"
             />
+            <Button type="submit">
+              Entrar na sala
+            </Button>
           </form>
         </div>
       </main>
